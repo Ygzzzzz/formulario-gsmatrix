b365b9e499fe4a33c88fb34179cb2cd516405c98
Ygzzzzz <107705427+Ygzzzzz@users.noreply.github.com>
2022-09-14 20:58:03 -0300

    correção do bug submit

--- loading.js
+++ loading.js
@@ -1,9 +1,5 @@
 function loading () {
     let carregou = document.getElementById('carregou')
 
-    if(carregou.style.display == 'none'){
-        carregou.style.display = 'block'
-    } else {
-        carregou.style.display = 'none'
-    }
+    carregou.style.display = 'block'
 }
