a3f9e67b1b72d0171a0aa3a6a1828b85e522462d
Ygzzzzz <107705427+Ygzzzzz@users.noreply.github.com>
2022-09-15 12:17:35 -0300

    validação de datanasc teste 1

--- loading.js
+++ loading.js
@@ -46,7 +46,28 @@ function mascaraCep() {
     } 
 }
 
-
+function validadata(){
+    var data = document.getElementById("inasc").value; // pega o valor do input
+    data = data.replace(/\//g, "-"); // substitui eventuais barras (ex. IE) "/" por hífen "-"
+    var data_array = data.split("-"); // quebra a data em array
+    
+    // para o IE onde será inserido no formato dd/MM/yyyy
+    if(data_array[0].length != 4){
+       data = data_array[2]+"-"+data_array[1]+"-"+data_array[0]; // remonto a data no formato yyyy/MM/dd
+    }
+    
+    // comparo as datas e calculo a idade
+    var hoje = new Date();
+    var nasc  = new Date(data);
+    var idade = hoje.getFullYear() - nasc.getFullYear();
+    var m = hoje.getMonth() - nasc.getMonth();
+    if (m < 0 || (m === 0 && hoje.getDate() < nasc.getDate())) idade--;
+    
+    if(idade < 18){
+       alert("Pessoas menores de 18 não podem se cadastrar.");
+       return false;
+    }
+}
 
 function loading () {
     let carregou = document.getElementById('carregou')
