528225a10353b3a1386a8dcf00a544fcddafa75c
Ygzzzzz <107705427+Ygzzzzz@users.noreply.github.com>
2022-09-15 12:57:11 -0300

    atualização

--- loading.js
+++ loading.js
@@ -1,76 +1,102 @@
-function mascaraCpf() {
-    let cpf = document.getElementById('icpf')
+function main() {
+  window.addEventListener("load", () => {
+    let form = document.querySelector("form#form");
 
-    if (cpf.value.length == 3 || cpf.value.length == 7) {
-        cpf.value += "."
-    } else if (cpf.value.length == 11) {
-        cpf.value += "-"
-    }
+    form.addEventListener("submit", (e) => {
+      let isValid = validadata();
+      console.log(isValid, e);
+      if (isValid) {
+        loading()
+      } else {
+        e.preventDefault();
+      }
+    });
+  });
+}
+
+main();
+
+function mascaraCpf() {
+  let cpf = document.getElementById("icpf");
+
+  if (cpf.value.length == 3 || cpf.value.length == 7) {
+    cpf.value += ".";
+  } else if (cpf.value.length == 11) {
+    cpf.value += "-";
+  }
 }
 
 function mascaraTelefone(event) {
-    let tecla = event.key;
-    let telefone = event.target.value.replace(/\D+/g, "");
+  let tecla = event.key;
+  let telefone = event.target.value.replace(/\D+/g, "");
 
-    if (/^[0-9]$/i.test(tecla)) {
-        telefone = telefone + tecla;
-        let tamanho = telefone.length;
+  if (/^[0-9]$/i.test(tecla)) {
+    telefone = telefone + tecla;
+    let tamanho = telefone.length;
 
-        if (tamanho >= 12) {
-            return false;
-        }
-        
-        if (tamanho > 10) {
-            telefone = telefone.replace(/^(\d\d)(\d{5})(\d{4}).*/, "($1) $2-$3");
-        } else if (tamanho > 5) {
-            telefone = telefone.replace(/^(\d\d)(\d{4})(\d{0,4}).*/, "($1) $2-$3");
-        } else if (tamanho > 2) {
-            telefone = telefone.replace(/^(\d\d)(\d{0,5})/, "($1) $2");
-        } else {
-            telefone = telefone.replace(/^(\d*)/, "($1");
-        }
-
-        event.target.value = telefone;
+    if (tamanho >= 12) {
+      return false;
     }
 
-    if (!["Backspace", "Delete"].includes(tecla)) {
-        return false;
+    if (tamanho > 10) {
+      telefone = telefone.replace(/^(\d\d)(\d{5})(\d{4}).*/, "($1) $2-$3");
+    } else if (tamanho > 5) {
+      telefone = telefone.replace(/^(\d\d)(\d{4})(\d{0,4}).*/, "($1) $2-$3");
+    } else if (tamanho > 2) {
+      telefone = telefone.replace(/^(\d\d)(\d{0,5})/, "($1) $2");
+    } else {
+      telefone = telefone.replace(/^(\d*)/, "($1");
     }
+
+    event.target.value = telefone;
+  }
+
+  if (!["Backspace", "Delete"].includes(tecla)) {
+    return false;
+  }
 }
 
 function mascaraCep() {
-    let cep = document.getElementById('icep')
+  let cep = document.getElementById("icep");
 
-    if (cep.value.length == 5) {
-        cep.value += "-"
-    } 
+  if (cep.value.length == 5) {
+    cep.value += "-";
+  }
 }
 
-function validadata(){
-    var data = document.getElementById("inasc").value; // pega o valor do input
-    data = data.replace(/\//g, "-"); // substitui eventuais barras (ex. IE) "/" por hífen "-"
-    var data_array = data.split("-"); // quebra a data em array
-    
-    // para o IE onde será inserido no formato dd/MM/yyyy
-    if(data_array[0].length != 4){
-       data = data_array[2]+"-"+data_array[1]+"-"+data_array[0]; // remonto a data no formato yyyy/MM/dd
-    }
-    
-    // comparo as datas e calculo a idade
-    var hoje = new Date();
-    var nasc  = new Date(data);
-    var idade = hoje.getFullYear() - nasc.getFullYear();
-    var m = hoje.getMonth() - nasc.getMonth();
-    if (m < 0 || (m === 0 && hoje.getDate() < nasc.getDate())) idade--;
-    
-    if(idade < 18){
-       alert("Pessoas menores de 18 não podem se cadastrar.");
-       return false;
-    }
+function validadata() {
+  var data = document.getElementById("inasc").value; // pega o valor do input
+  data = data.replace(/\//g, "-"); // substitui eventuais barras (ex. IE) "/" por hífen "-"
+  var data_array = data.split("-"); // quebra a data em array
+
+  // para o IE onde será inserido no formato dd/MM/yyyy
+  if (data_array[0].length != 4) {
+    data = data_array[2] + "-" + data_array[1] + "-" + data_array[0]; // remonto a data no formato yyyy/MM/dd
+  }
+
+  // comparo as datas e calculo a idade
+  var hoje = new Date();
+  var nasc = new Date(data);
+  var idade = hoje.getFullYear() - nasc.getFullYear();
+  var m = hoje.getMonth() - nasc.getMonth();
+  if (m < 0 || (m === 0 && hoje.getDate() < nasc.getDate())) idade--;
+
+  if (idade < 18) {
+    alert("Pessoas menores de 18 não podem se cadastrar.");
+    return false;
+  }
+  return true
 }
 
-function loading () {
-    let carregou = document.getElementById('carregou')
+// function submitForm(e) {
+//     if(validadata()) {
+//         e.preventDefault()
+//         return
+//     }
+// }
 
-    carregou.style.display = 'block'
+function loading() {
+  let carregou = document.getElementById("carregou");
+
+  carregou.style.display = "block";
 }
