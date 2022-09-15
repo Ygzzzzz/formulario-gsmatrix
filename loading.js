678302b1590e58e11ecc84fa1c6e091350102c50
Ygzzzzz <107705427+Ygzzzzz@users.noreply.github.com>
2022-09-15 10:56:34 -0300

    atualização de campos cpf e telefone

--- loading.js
+++ loading.js
@@ -1,3 +1,45 @@
+function mascaraCpf() {
+    let cpf = document.getElementById('icpf')
+
+    if (cpf.value.length == 3 || cpf.value.length == 7) {
+        cpf.value += "."
+    } else if (cpf.value.length == 11) {
+        cpf.value += "-"
+    }
+}
+
+function mascaraTelefone(event) {
+    let tecla = event.key;
+    let telefone = event.target.value.replace(/\D+/g, "");
+
+    if (/^[0-9]$/i.test(tecla)) {
+        telefone = telefone + tecla;
+        let tamanho = telefone.length;
+
+        if (tamanho >= 12) {
+            return false;
+        }
+        
+        if (tamanho > 10) {
+            telefone = telefone.replace(/^(\d\d)(\d{5})(\d{4}).*/, "($1) $2-$3");
+        } else if (tamanho > 5) {
+            telefone = telefone.replace(/^(\d\d)(\d{4})(\d{0,4}).*/, "($1) $2-$3");
+        } else if (tamanho > 2) {
+            telefone = telefone.replace(/^(\d\d)(\d{0,5})/, "($1) $2");
+        } else {
+            telefone = telefone.replace(/^(\d*)/, "($1");
+        }
+
+        event.target.value = telefone;
+    }
+
+    if (!["Backspace", "Delete"].includes(tecla)) {
+        return false;
+    }
+}
+
+
+
 function loading () {
     let carregou = document.getElementById('carregou')
 
